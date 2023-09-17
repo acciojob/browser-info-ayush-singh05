4f41d9e3ebecc7433076042b52da72dd50ef39ed
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-09-17 12:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let x = navigator.appName;
+let v = navigator.appVersion;
+console.log(x,v)
+let tar = document.getElementById("browser-info").innerHTML = "you are using "+ x + " version "+ v;
